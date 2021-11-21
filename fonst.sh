f5c6b964206568866ed20efd6ff21ea47682268c
SirNoob97 <martinald467@gmail.com>
2021-11-21 16:39:50 +0100

    script to retrive the true font name installed in specified directory

---
+++ fonst.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DIRECTORY="${1:-$HOME/.local/share/fonts/}"
+AWK_PATTERN='
+BEGIN {
+  FS = ":"
+}
+{
+  line[NR] = $2
+}
+END{
+  isort(line, NR)
+  filter(line)
+} 
+
+function filter(A, hold) {
+  for(l in A) {
+    hold[A[l]] = ""
+  }
+  
+  for(name in hold) print name
+}
+
+function isort(A, n, i, j, hold) {
+  for(i = 2; i <= n; i++){
+    hold = A[j = i]
+    while(A[j-1] > hold){
+      j--
+      A[j + 1] = A[j]
+    } A[j] = hold
+  }
+}
+'
+echo -e "Available fonts installed in $DIRECTORY\n"
+fc-list | grep "$DIRECTORY" | awk "$AWK_PATTERN" | grep -vE '^$|[iI]con[s]'
