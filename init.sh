ade05f972a30bc08fa40c6d2e3e2572ffdd47144
SirNoob97 <martinald467@gmail.com>
2021-12-22 15:44:27 +0100

    replace nmtui command with nmcli options

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 nmcli radio wifi on
 
-nmtui && sudo apt update
+[ "enabled" == "$(nmcli radio wifi)" ] && nmcli device connect wlp2s0 --ask
+
+nmcli device show wlp2s0 | grep 'GENERAL\.STATE:' | awk '{ if("(connected)" == $3"") exit 0 }' && sudo apt update
 
 ssh-add
