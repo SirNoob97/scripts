814341e9684b9fe3723f344f223580b53b7677ae
SirNoob97 <martinald467@gmail.com>
2022-01-15 16:36:57 +0100

    refactor extract syslink loop to a function

--- install.sh
+++ install.sh
@@ -4,9 +4,13 @@ export install_dir="${1:-$HOME/.local/bin}"
 
 [ ! -d $install_dir ] && mkdir -p $install_dir
 
-scripts=$(find $PWD -path */postgresql -prune -o -type f -perm '-u=x' \( -name '*.sh' -not -name '*backup*' -not -name '*install*' \) -printf "%h/%f\n")
-
-for s in $scripts; do
+function __install {
+  for s in $1; do
     name=$(basename $s)
     ln --force --symbolic $s $install_dir/${name%.sh}
-done
+  done
+}
+
+export -f __install
+
+scripts=$(find $PWD -path */postgresql -prune -o -type f -perm '-u=x' \( -name '*.sh' -not -name '*backup*' -not -name '*install*' \) -printf "%h/%f\n")
