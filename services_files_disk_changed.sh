f2223187387996c6b437319aa121ff84cb4e9bc2
SirNoob97 <martinald467@gmail.com>
2022-01-26 20:52:10 +0100

    refactor reduce repetitive code

--- services_files_disk_changed.sh
+++ services_files_disk_changed.sh
@@ -2,17 +2,20 @@
 
 # list the services, sockets or timers that need a daemon reload
 
-echo "---Services---"
-for s in $(systemctl list-unit-files | grep 'enabled' | grep '.service' | awk '{print $1}'); do
-  systemctl status $s 2>&1 | grep 'systemctl daemon-reload' | awk '{print $11}'
-done
+__list_enabled_units() {
+  systemctl list-unit-files --no-pager --no-legend -at $1 --state enabled \
+    | awk '{print $1}'
+}
 
-echo -e "\n---Sockets---"
-for s in $(systemctl list-unit-files | grep 'enabled' | grep '.socket' | awk '{print $1}'); do
-  systemctl status $s 2>&1 | grep 'systemctl daemon-reload' | awk '{print $11}'
-done
+__list_services_that_need_daemon_reload() {
+  systemctl status $1 2>&1 | grep 'systemctl daemon-reload' | awk '{print $11}'
+}
 
-echo -e "\n---Timers---"
-for s in $(systemctl list-unit-files | grep 'enabled' | grep '.timer' | awk '{print $1}'); do
-  systemctl status $s 2>&1 | grep 'systemctl daemon-reload' | awk '{print $11}'
+
+for ut in "service" "socket" "timer"; do
+  echo "---${ut^}s---"
+  for s in $(__list_enabled_units $ut); do
+    __list_services_that_need_daemon_reload $s
+  done
+  echo ""
 done
