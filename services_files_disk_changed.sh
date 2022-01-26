81325618eff0437761c46ed074dda9de592fba59
SirNoob97 <martinald467@gmail.com>
2022-01-26 17:56:39 +0100

    add socket and timer loops

--- services_files_disk_changed.sh
+++ services_files_disk_changed.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# list the services that need a daemon reload
+# list the services, sockets or timers that need a daemon reload
 
+echo "---Services---"
 for s in $(systemctl list-unit-files | grep 'enabled' | grep '.service' | awk '{print $1}'); do
+  systemctl status $s 2>&1 | grep 'systemctl daemon-reload' | awk '{print $11}'
+done
+
+echo -e "\n---Sockets---"
+for s in $(systemctl list-unit-files | grep 'enabled' | grep '.socket' | awk '{print $1}'); do
+  systemctl status $s 2>&1 | grep 'systemctl daemon-reload' | awk '{print $11}'
+done
+
+echo -e "\n---Timers---"
+for s in $(systemctl list-unit-files | grep 'enabled' | grep '.timer' | awk '{print $1}'); do
   systemctl status $s 2>&1 | grep 'systemctl daemon-reload' | awk '{print $11}'
 done
