bf3bbe8027ce1677d534de5de2eef5cffc73c4ee
SirNoob97 <martinald467@gmail.com>
2022-01-27 16:58:30 +0100

    simplify, now all units are listed

--- services_files_disk_changed.sh
+++ services_files_disk_changed.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 __list_not_found_units() {
-  systemctl list-units --no-pager --no-legend --all --type $1 --state not-found \
+  systemctl list-units --no-pager --no-legend --all --state not-found \
     | awk '{print $1}'
 }
 
-__list_enabled_units() {
-  systemctl list-unit-files --no-pager --no-legend --all --type $1 --state enabled \
-    | awk '{print $1}'
+__list_all_units() {
+  systemctl list-unit-files --no-pager --no-legend --all \
+    | awk '{if(length($1) > 1) print $1}'
 }
 
 __list_services_that_need_daemon_reload() {
-  systemctl status $1 2>&1 | grep 'systemctl daemon-reload' | awk '{print $11}'
+  systemctl status $1 2>&1 | grep 'systemctl daemon-reload' \
+    | awk '{if(length($11) > 1) print $11}'
 }
 
 
-for unit_type in "service" "socket" "timer"; do
-  echo "---${unit_type^}s---"
-  for unit in $(__list_enabled_units "$unit_type"); do
-    __list_services_that_need_daemon_reload $unit
-  done
-  echo -e "\n---Not found ${unit_type}s---"
-  __list_not_found_units $unit_type
-  echo ""
+echo "---Units that need a daemon reload---"
+for unit in $(__list_all_units); do
+  __list_services_that_need_daemon_reload $unit
 done
+
+echo -e "\n---Not found units---"
+ __list_not_found_units $unit_type
